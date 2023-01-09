19be6ffd23baa7385987625d96ad52a4e78108f7
Alena <116582154+GruzdevaAlena@users.noreply.github.com>
2023-01-09 19:55:50 +0300

    Add files via upload

---
+++ Lab01/Lab1_1/Lab1/nForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class nForm : Form
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+        private void nForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (nclose) return;
+            e.Cancel = true;
+            Hide();
+        }
+        private bool nclose = false;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+        public new void Close()
+        {
+            nclose = true;
+            base.Close();
+        }
+
+    }
+}
