5660dc9179856d8a61a60c8d60af6f0a4e5dcd31
Alena <116582154+GruzdevaAlena@users.noreply.github.com>
2023-05-30 19:16:30 +0300

    Add files via upload

---
+++ CourseC++/08. Lab/Lab8Zachet/Lab8Zachet/Lab8Zachet.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include "Time.h"
+using namespace std;
+int main()
+{
+    system("chcp 1251");
+
+    Time *time1 = new Time(5,-10,61);
+    Time *time2 = new Time(4, 12, 59);
+    Time *time3 = new Time();
+    time1->Display();
+    cout << endl;
+    time2->Display();
+    cout << endl;
+    time3->Display();
+    cout << endl;
+    *time3 = time3->sumTime(*time1, *time2);
+    time3->Display();
+    delete time1;
+    delete time2;
+    delete time3;
+}
+
